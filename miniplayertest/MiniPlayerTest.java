329460cd6197effdda88ca8fc613acda70ff04c2
sandra_c <chavezsandra0498@gmail.com>
2020-12-09 02:37:08 -0800

    Create MiniPlayerTest.java

---
+++ miniplayertest/MiniPlayerTest.java
@@ -0,0 +1,21 @@
+
+package miniplayertest;
+
+/**
+ *
+ * @author Dr. Hoffman
+ */
+public class MiniPlayerTest {
+
+    /**
+     * @param args the command line arguments
+     */
+        
+    public static void main(String[] args) {
+        MiniPlayerGUI   gui = new MiniPlayerGUI();
+        
+        gui.go();
+
+    }
+    
+}
